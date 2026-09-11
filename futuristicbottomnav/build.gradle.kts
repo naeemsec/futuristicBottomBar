70e970f4e83b93f0074c3f2eb469c03f726c99c8
naeemsec <bilujee512@gmail.com>
2026-09-11 12:00:14 +0500

    Build errors solved

--- futuristicbottomnav/build.gradle.kts
+++ futuristicbottomnav/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
+    id("maven-publish")
 }
 
 android {
@@ -28,13 +29,27 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
 }
 
 dependencies {
-    //implementation(project(":futuristicbottomnav"))
     implementation(libs.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
+//
+//afterEvaluate {
+//    publishing {
+//        publications {
+//            create<MavenPublication>("release") {
+//                from(components["release"])
+//            }
+//        }
+//    }
+//}
